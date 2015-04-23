00a1f3c369b4a3d23041b4d1376b7f54fd5a3f7a
andrew & james robert williams <student@epicodus.com>
2015-04-23 09:32:15 -0700

    Added second spec to check amount of quarters

--- spec/coin_count_spec.rb
+++ spec/coin_count_spec.rb
@@ -5,5 +5,10 @@ require('coin_count')
 describe('Fixnum#coin_count') do
  it('takes the amount of change and prints on page') do
    expect(3.coin_count()).to(eq(3))
- end  
+ end
+
+  it('takes the amount of change and prints out the number of quarters') do
+    expect(75.coin_count()).to(eq("There are 3 quarters"))
+  end
+    
 end
